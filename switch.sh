4b9648ca8332328223889947276d03b6b0d4fbc7
Michael Bruenisholz <info@michael-bruenisholz.ch>
2015-05-03 12:35:17 +0200

    added output to statefile

--- switch.sh
+++ switch.sh
@@ -7,4 +7,10 @@ cd `dirname $0`
 username="admin"
 password="1234"
 
-curl -d @$2.xml http://$username:$password@$1:10000/smartplug.cgi
+output=`curl --silent -d @$2.xml http://$username:$password@$1:10000/smartplug.cgi`
+
+if [ "$2" = "status" ]; then
+	echo $output | grep -oPm1 "(?<=<Device.System.Power.State>)[^<]+" > state_$1.txt
+else
+	echo $2 > state_$1.txt
+fi
